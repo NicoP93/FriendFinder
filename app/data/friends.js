4b3004eed3a74ba480598b45fa3c940e39da85e6
NicoP93 <nicopcrr@gmail.com>
2019-04-09 15:30:05 -0600

    Heroku build push

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+var friends = [
+    {
+      name: "Dwight",
+      photo: "https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg",
+      scores: [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+    },
+    {
+      "name":"Jim",
+      "photo":"https://upload.wikimedia.org/wikipedia/en/7/7e/Jim-halpert.jpg",
+      "scores":[
+          5,
+          5,
+          4,
+          4,
+          5,
+          5,
+          4,
+          5,
+          4,
+          5
+        ]
+    },
+    {
+      "name":"Ryan",
+      "photo":"https://upload.wikimedia.org/wikipedia/en/9/91/Ryan_Howard_%28The_Office%29.jpg",
+      "scores":[
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2
+        ]
+    },
+    {
+      "name":"Pam",
+      "photo":"https://upload.wikimedia.org/wikipedia/en/6/67/Pam_Beesley.jpg",
+      "scores":[
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          2,
+          5,
+          4,
+          3
+        ]
+    },
+    {
+      "name":"Michael",
+      "photo":"https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png",
+      "scores":[
+          5,
+          1,
+          4,
+          4,
+          5,
+          5,
+          2,
+          5,
+          4,
+          1
+        ]
+    },
+  ]
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+// Tells node that we are creating an "express" server
+
+// Sets an initial port. We"ll use this later in our listener
+
+
+// BodyParser makes it possible for our server to interpret data sent to it.
+// The code below is pretty standard.
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require(path.join(__dirname, '/app/routing/apiroutes.js'))(app);
+require(path.join(__dirname, '/app/routing/htmlRoutes.js'))(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
